a359683d2ea7f425bc0c0a18246c9123035c1c2e
oskar <reoskaro434@gmail.com>
2023-07-30 18:14:51 +0200

    Added pipfile

--- src/main.py
+++ src/main.py
@@ -1,18 +1,9 @@
-import requests
-
-# URL for the Reddit API
-url = "https://www.reddit.com/subreddits/popular.json"
-
-# Send a GET request to the Reddit API
-response = requests.get(
-    url,
-    headers={"User-agent": "Mozilla/5.0"}
+import praw
+CLIENT_ID = "[YOUR ID KEY HERE]"
+CLIENT_SECRET = "[YOUR SECRET KEY HERE]"
+USER_AGENT = "[YOUR USERNAME HERE]"
+reddit = praw.Reddit(
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
+    user_agent=USER_AGENT,
 )
-
-# Parse the JSON response
-data = response.json()
-
-# Iterate over the subreddits in the response
-for subreddit in data["data"]["children"]:
-    # Print the name of each subreddit
-    print(subreddit["data"]["display_name"])
