bf42ab5e7be1429ac827cca572dc671020e78365
oskar <reoskaro434@gmail.com>
2023-08-05 19:40:56 +0200

    improved script to ensure that data is saved immediately

--- src/main.py
+++ src/main.py
@@ -1,3 +1,3 @@
 from src.reddit_data_collector import save_subreddit_data
 
-save_subreddit_data('health', 400)
+save_subreddit_data('health', 4)
--- src/reddit_data_collector.py
+++ src/reddit_data_collector.py
@@ -17,16 +17,26 @@ REDDIT = praw.Reddit(
 )
 
 
-def save_subreddit_data(subreddit_name, submission_number):
+def __configure_logger(subreddit_name):
     current_datetime = datetime.datetime.now()
-    formatted_datetime = current_datetime.strftime('%Y-%m-%d_%H-%M-%S')
+    formatted_datetime = current_datetime.strftime('%Y_%m_%d__%H:%M:%S')
+
+    log_file_name = f"./logs/{subreddit_name}-{formatted_datetime}.log"
+
+    with open(log_file_name, 'a') as f:
+        f.write('')
+
     logging.basicConfig(
-        level=logging.DEBUG,  # Set the logging level
-        filename=f'app_{formatted_datetime}.log',  # Specify the filename for the log file
-        filemode='a',  # 'a' means append mode
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        handlers=[
+            logging.FileHandler(log_file_name),
+            logging.StreamHandler()
+        ]
     )
 
+
+def __set_up_subreddit(subreddit_name, submission_number):
     subreddit = REDDIT.subreddit(subreddit_name)
 
     name = subreddit.display_name
@@ -37,38 +47,43 @@ def save_subreddit_data(subreddit_name, submission_number):
     logging.info((name, id_, public_description, created_at))
     logging.info(f'Save {submission_number} posts from the {subreddit_name}')
 
+    return subreddit
+
+
+def save_subreddit_data(subreddit_name, submission_number):
+    __configure_logger(subreddit_name)
+    subreddit = __set_up_subreddit(subreddit_name, submission_number)
+    file_path = f"./reddit_data/{subreddit_name}.json"
     counter = 0
+
     try:
-        with open(f"{subreddit_name}.json", "a") as file:
-            file.write('[\n')
-            for submission in subreddit.new(limit=submission_number):
-                counter += 1
+        with open(file_path, "a") as f:
+            f.write('[\n')
 
-                title = submission.title
-                link = submission.url
-                author = submission.author_fullname
-                score = submission.score
-                created = submission.created
+        for submission in subreddit.new(limit=submission_number):
+            counter += 1
 
-                data = {
-                    'title': title,
-                    'link': link,
-                    'author': author,
-                    'score': score,
-                    'created': created,
-                }
+            data = {
+                'title': submission.title,
+                'link': submission.url,
+                'author': submission.author_fullname,
+                'score': submission.score,
+                'created': submission.created,
+            }
 
-                logging.info(f"saving post nr: {counter} from: {subreddit_name}")
+            logging.info(f"saving post nr: {counter} from: {subreddit_name}")
 
+            with open(file_path, "a") as f:
                 if counter > 1:
-                    file.write(',\n')
+                    f.write(',\n')
 
-                json.dump(data, file, indent=2)
+                json.dump(data, f, indent=2)
 
-                if counter % 100 == 0:  # API rate limits is 100 per 60 seconds
-                    logging.warning(f'This is a {counter} waiting about 1 minute to continue')
-                    time.sleep(60)
-            file.write('\n]')
+            if counter % 100 == 0:  # API rate limits is 100 per 60 seconds
+                logging.warning(f'This is a {counter} waiting about 1 minute to continue')
+                time.sleep(60)
+        with open(file_path, "a") as f:
+            f.write('\n]')
 
     except Exception as ex:
         logging.error(f'Oh no! An error occurred while saving post nr:{counter} from {subreddit_name}')
