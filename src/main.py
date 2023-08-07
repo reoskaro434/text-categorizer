f83a54d2fa1a0970dd10cf46b3d19f632fc011cd
oskar <reoskaro434@gmail.com>
2023-08-07 22:25:49 +0200

    added resanter-logger to the project

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from reddit_data_collector import RedditDataCollector
-from logger.logger import Logger
+from resanter_logger.re_logger import ReLogger
 from timer.timer import Timer
 
 subreddit_list = [
@@ -37,7 +37,7 @@ subreddit_list = [
 ]
 
 for subreddit_name in subreddit_list:
-    reddit_logger = Logger(subreddit_name)
+    reddit_logger = ReLogger(subreddit_name)
     reddit_timer = Timer(reddit_logger)
 
     RedditDataCollector(reddit_timer, reddit_logger).save_subreddit_data(subreddit_name, 4)
